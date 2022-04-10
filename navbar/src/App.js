959996bf9239147ea88a112bf586934acdf68f15
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-04-10 16:16:04 +0530

    react navbar

---
+++ navbar/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
