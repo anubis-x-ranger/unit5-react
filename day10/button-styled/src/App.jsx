841eefe2df0d700962c5e56945d8cd6c65553997
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-04-22 10:47:03 +0530

    final update

--- day10/button-styled/src/App.jsx
+++ day10/button-styled/src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import styled from 'styled-components'
 import './App.css'
-
+import Button from "../src/components/Button"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <ButtonWrapper>
+      <Button background="#2490fd" color="white" >Primary Button</Button>
+      <Button background="white" color="black" bordertype="solid" borderColor="gray">Default Button</Button>
+      <Button background="white" color="black" bordertype="dashed" borderColor="gray">Dashed Button</Button>
+      <Button background="white" color="black">Text Button</Button>
+      <Button background="white" color="#2490fd">Link Button</Button>
+    </ButtonWrapper>
      
-    </div>
+    
   )
 }
 
 export default App
+
+
+const ButtonWrapper=styled.div`
+ border:1px solid transparent;
+ height:180px;
+ width:600px;
+ margin:auto;
+ justify-content:center;
+ display:grid;
+ grid-template-columns: repeat(3,1fr);
+ margin-top: 10px;
+`;
--- day10/button-styled/src/components/Button.jsx
+++ day10/button-styled/src/components/Button.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-
-const Button = () => {
-  return (
-    <button>
-        
-    </button>
-  )
-}
-
+import styled from "styled-components"
+const Button=styled.button`
+    height:40px;
+    width:150px;
+    border:none;
+    font-family: "Proxima Nova Regular", sans-serif;
+    border-radius: 5px;
+    margin-left: 10px;
+    margin: 10px 0px;
+    cursor:pointer;
+    background-color:${(props)=>props.background};
+    color:${(props)=>props.color};
+    border-style: ${(props)=>props.bordertype=== "dashed" ? "dashed": props.bordertype=== "solid" ? "solid" : "none"};
+    border-color: ${(props)=>props.borderColor=== "gray"  ? "gray" : "none"};
+`;
 export default Button
