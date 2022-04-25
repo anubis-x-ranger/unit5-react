7cf51bc3b9a67b18245f6d65d1acf54842665495
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-04-25 13:09:24 +0530

    update

---
+++ useEffect/Timer-Inc/src/components/Timer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useEffect } from 'react';
+const Timer = () => {
+    useEffect(() => {
+      first
+    
+      return () => {
+        second
+      }
+    }, [third]);
+    
+  return (
+    <div>Timer</div>
+  )
+}
+
+export default Timer
